4eedecfd215a1aaa90c40e00e3bf882f00b5e4bd
Dimitar Mitev <d.mitev920123@gmail.com>
2025-06-19 19:30:18 +0300

    feat: login functionality in server

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,8 +2,8 @@ import jwt from "jsonwebtoken";
 
 export const adminLogin = async (req, res) => {
   try {
-    const { email, password } = res.body;
-
+    const { email, password } = req.body;
+    console.log(email, password);
     if (
       email !== process.env.ADMIN_EMAIL ||
       password !== process.env.ADMIN_PASSWORD
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 
+import adminRouter from "./routes/adminRoutes.js";
+
 const app = express();
 
 await connectDB();
@@ -10,9 +12,12 @@ await connectDB();
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" });
 });
+// Admin Routes
+app.use("/api/admin", adminRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
