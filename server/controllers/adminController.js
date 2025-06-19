3b5a563f856500cd6ade5876181d929901ddf83d
Dimitar Mitev <d.mitev920123@gmail.com>
2025-06-19 19:16:28 +0300

    feat: admin controller

---
+++ server/controllers/adminController.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = res.body;
+
+    if (
+      email !== process.env.ADMIN_EMAIL ||
+      password !== process.env.ADMIN_PASSWORD
+    )
+      return res
+        .status(400)
+        .json({ message: "Invalid credentials", success: false });
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res
+      .status(200)
+      .json({ message: "Admin logged in successfully", success: true, token });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
