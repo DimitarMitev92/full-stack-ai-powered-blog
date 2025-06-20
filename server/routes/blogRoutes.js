98d4966e327a7ea4d6533c8d52a1246204b08d50
Dimitar Mitev <d.mitev920123@gmail.com>
2025-06-20 17:17:59 +0300

    feat: add authentication middleware to blog routes

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { addBlog } from "../controllers/blogController.JS";
 import upload from "../middleware/multer";
+import auth from "../middleware/auth";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", upload.single("image"), addBlog, addBlog);
+blogRouter.post("/add", auth, upload.single("image"), addBlog);
+
+export default blogRouter;
