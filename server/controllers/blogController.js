69a06f31a507161941a4b7caa156587907ea9322
Dimitar Mitev <d.mitev920123@gmail.com>
2025-06-20 17:09:34 +0300

    feat: setup imagekit and blog controller

---
+++ server/controllers/blogController.js
@@ -0,0 +1,55 @@
+import fs from "fs";
+import imagekit from "../configs/imageKit.js";
+
+export const addBlog = async (req, res) => {
+  try {
+    const { title, subTitle, description, category, image, isPublished } =
+      JSON.parse(req.body.blog);
+    const imageFile = req.file;
+
+    // Check if all fields are provided
+    if (!title || !description || !category || !imageFile || !isPublished) {
+      return res
+        .status(400)
+        .json({ message: "Missing required fields", success: false });
+    }
+
+    const fileBuffer = fs.readFileSync(imageFile.path);
+
+    // Upload the image to ImageKit
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/quickblog",
+    });
+
+    // Optimizing the image
+    const optimizedImageUrl = await imagekit.url({
+      path: response.filePath,
+      transformation: [
+        {
+          quality: "auto",
+        },
+        {
+          format: "webp",
+        },
+        {
+          width: 1280,
+        },
+      ],
+    });
+
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image: optimizedImageUrl,
+      isPublished,
+    });
+
+    res.status(200).json({ message: "Blog added successfully", success: true });
+  } catch (error) {
+    res.status(500).json({ message: error.message, success: false });
+  }
+};
