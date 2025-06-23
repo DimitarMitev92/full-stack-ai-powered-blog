2ae8e8f9cfd93b6133238abbd1bedc16ade67290
Dimitar Mitev <d.mitev920123@gmail.com>
2025-06-23 17:41:35 +0300

    feat: add react-hot-toast for notifications and enhance login flow with token management

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
@@ -12,6 +13,24 @@ export const AppProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [blogs, setBlogs] = useState([]);
   const [input, setInput] = useState("");
+
+  const fetchBlogs = async () => {
+    try {
+      const { data } = await axios.get("/api/blog/all");
+      data.success ? setBlogs(data.blogs) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+    const token = localStorage.getItem("token");
+    if (token) {
+      setToken(token);
+      axios.defaults.headers.common["Authorization"] = token;
+    }
+  }, []);
 
   const value = {
     axios,
