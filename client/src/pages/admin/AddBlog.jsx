6b20222a990dd5fb2efe519a920b8229df483e65
Dimitar Mitev <d.mitev920123@gmail.com>
2025-06-24 11:44:33 +0300

    feat: implement blog addition functionality with form handling and notifications

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -1,21 +1,60 @@
 import React, { useEffect, useRef, useState } from "react";
 import { assets, blogCategories } from "../../assets/assets";
 import Quill from "quill";
+import { useAppContext } from "../../context/AppContext";
 
 const AddBlog = () => {
+  const { axios } = useAppContext();
+
+  const [isAdding, setIsAdding] = useState(false);
+
   const editorRef = useRef(null);
   const quillRef = useRef(null);
 
   const [image, setImage] = useState(false);
   const [title, setTitle] = useState("");
-  const [subtitle, setSubtitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
   const [category, setCategory] = useState("");
   const [isPublished, setIsPublished] = useState(false);
 
   const generateContent = async () => {};
 
   const onSubmitHandler = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      setIsAdding(true);
+
+      const blog = {
+        title,
+        subTitle,
+        description: quillRef.current.root.innerHTML,
+        category,
+        isPublished,
+      };
+
+      const formData = new FormData();
+      formData.append("blog", JSON.stringify(blog));
+      formData.append("image", image);
+
+      const { data } = await axios.post("/api/blog/add", formData);
+
+      if (data.success) {
+        toast.success(data.message);
+        setIsAdding(false);
+        setTitle("");
+        setSubTitle("");
+        setCategory("");
+        setIsPublished(false);
+        setImage(false);
+        quillRef.current.root.innerHTML = "";
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsAdding(false);
+    }
   };
 
   useEffect(() => {
@@ -63,8 +102,8 @@ const AddBlog = () => {
           placeholder="Type here"
           required
           className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
-          value={subtitle}
-          onChange={(e) => setSubtitle(e.target.value)}
+          value={subTitle}
+          onChange={(e) => setSubTitle(e.target.value)}
         />
 
         <p className="mt-4">Blog description</p>
@@ -103,10 +142,11 @@ const AddBlog = () => {
           />
         </div>
         <button
+          disabled={isAdding}
           type="submit"
           className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm"
         >
-          Add blog
+          {isAdding ? "Adding..." : "Add blog"}
         </button>
       </div>
     </form>
