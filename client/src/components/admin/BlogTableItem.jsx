c603e32cf5cc28cb38427e6870e534ff4f836dd0
Dimitar Mitev <d.mitev920123@gmail.com>
2025-06-24 12:23:30 +0300

    feat: implement blog deletion and publishing toggle functionality with notifications

--- client/src/components/admin/BlogTableItem.jsx
+++ client/src/components/admin/BlogTableItem.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const BlogTableItem = ({ blog, fetchBlogs, index }) => {
   const { title, createdAt } = blog;
   const BlogDate = new Date(createdAt);
+
+  const { axios } = useAppContext();
+
+  const deleteBlog = async () => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this blog?"
+    );
+
+    if (!confirm) return;
+
+    try {
+      const { data } = await axios.post(`/api/blog/delete`, { id: blog._id });
+      if (data.success) {
+        toast.success(data.message);
+        fetchBlogs();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const togglePublish = async () => {
+    try {
+      const { data } = await axios.post("/api/blog/toggle-publish", {
+        id: blog._id,
+      });
+      if (data.success) {
+        toast.success(data.message);
+        fetchBlogs();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <tr className="border-y border-gray-300">
       <th className="px-2 py-4">{index}</th>
@@ -19,13 +60,17 @@ const BlogTableItem = ({ blog, fetchBlogs, index }) => {
         </p>
       </td>
       <td className="px-2 py-4 flex text-xs gap-3">
-        <button className="border px-2 py-0.5 mt-1 rounded cursor-pointer">
+        <button
+          className="border px-2 py-0.5 mt-1 rounded cursor-pointer"
+          onClick={togglePublish}
+        >
           {blog.isPublished ? "Unpublished" : "Published"}
         </button>
         <img
           src={assets.cross_icon}
           className="w-8 hover:scale-110 transition-all cursor-pointer"
           alt=""
+          onClick={deleteBlog}
         />
       </td>
     </tr>
